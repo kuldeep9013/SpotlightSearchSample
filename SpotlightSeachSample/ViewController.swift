a04c32697b7198f93e89fc5868d78b7a4a308b61
kuldeep9013 <kuldeepsolanki9013@gmail.com>
2025-04-23 17:16:54 +0530

    Initial Commit

---
+++ SpotlightSeachSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotlightSeachSample
+//
+//  Created by Kuldeep Solanki on 23/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
