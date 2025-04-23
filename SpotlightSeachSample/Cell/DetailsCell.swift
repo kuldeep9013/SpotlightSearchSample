11a4d0b3f644a5b23777d8467d2a8172031dd71d
kuldeep9013 <kuldeepsolanki9013@gmail.com>
2025-04-23 17:42:42 +0530

    Initial commit

---
+++ SpotlightSeachSample/Cell/DetailsCell.swift
@@ -0,0 +1,17 @@
+//
+//  DetailsCell.swift
+//  SpotlightSeachSample
+//
+//  Created by Kuldeep Solanki on 23/04/25.
+//
+
+import UIKit
+
+class DetailsCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
